2d42d8cc4b37fec26003f2abada8967f6b66dfb8
mobin <mobinmohammadi4321@gmail.com>
2025-12-19 00:04:54 +0330

    updated

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { ThemeProvider } from "./assets/Context/Context.tsx";
-import App from "./App.tsx";
-createRoot(document.getElementById("root")).render(_jsx(React.StrictMode, { children: _jsx(ThemeProvider, { children: _jsx(App, {}) }) }));
