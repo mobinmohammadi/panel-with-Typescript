056c3fa4515c3aee51b224a1e9ffe41a9a8455dc
mobin <mobinmohammadi4321@gmail.com>
2025-12-26 00:40:37 +0330

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react() , tailwindcss() , line],
+  plugins: [react() , tailwindcss() ,],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
